92833eafccf546b83e38ea0d986867f2834e419f
WyattAu <wyatt_au@protonmail.com>
2026-08-31 18:27:40 +0100

    chore: add target/ to .gitignore

--- target/debug/deps/breaker-3b320f2760dfc835.d
+++
@@ -1,9 +0,0 @@
-/home/wyatt/dev/src/github.com/WyattAu/breaker/target/debug/deps/breaker-3b320f2760dfc835.d: src/lib.rs src/config.rs src/error.rs src/metrics.rs src/state.rs
-
-/home/wyatt/dev/src/github.com/WyattAu/breaker/target/debug/deps/breaker-3b320f2760dfc835: src/lib.rs src/config.rs src/error.rs src/metrics.rs src/state.rs
-
-src/lib.rs:
-src/config.rs:
-src/error.rs:
-src/metrics.rs:
-src/state.rs:
--- target/debug/deps/breaker-fffa1f086056894a.d
+++
@@ -1,9 +0,0 @@
-/home/wyatt/dev/src/github.com/WyattAu/breaker/target/debug/deps/breaker-fffa1f086056894a.d: src/lib.rs src/config.rs src/error.rs src/metrics.rs src/state.rs
-
-/home/wyatt/dev/src/github.com/WyattAu/breaker/target/debug/deps/libbreaker-fffa1f086056894a.rmeta: src/lib.rs src/config.rs src/error.rs src/metrics.rs src/state.rs
-
-src/lib.rs:
-src/config.rs:
-src/error.rs:
-src/metrics.rs:
-src/state.rs:
